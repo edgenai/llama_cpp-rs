5dd003f934ea1261e95683028fa128a6842e4dc3
Pedro Valente <pedro.amaral.valente@gmail.com>
2024-02-08 17:12:46 +0000

    wip: further expanded the build script

--- crates/llama_cpp_sys/build.rs
+++ crates/llama_cpp_sys/build.rs
@@ -97,130 +97,158 @@ fn compile_bindings(out_path: &Path) {
         .expect("Couldn't write bindings!");
 }
 
-/// Add platform appropriate flags present in all compilation configurations.
-fn push_common_flags(cx_flags: &mut Vec<&str>, cxx_flags: &mut Vec<&str>) {
+/// Add platform appropriate flags and definitions present in all compilation configurations.
+fn push_common_flags(cx: &mut Build, cxx: &mut Build) {
+    cx.static_flag(true).cpp(false).std("c11");
+    cxx.static_flag(true).cpp(true).std("c++14"); // MSVC does not support C++11
+
     if cfg!(target_family = "unix") {
-        cx_flags.push("-std=c11");
-        cx_flags.push("-pthread");
-        cx_flags.push("-Wall");
-        cx_flags.push("-Wextra");
-        cx_flags.push("-Wpedantic");
-        cx_flags.push("-Wcast-qual");
-        cx_flags.push("-Wdouble-promotion");
-        cx_flags.push("-Wshadow");
-        cx_flags.push("-Wstrict-prototypes");
-        cx_flags.push("-Wpointer-arith");
-
-        cxx_flags.push("-std=c++11");
-        cxx_flags.push("-fPI");
-        cxx_flags.push("-pthread");
-        cxx_flags.push("-Wall");
-        cxx_flags.push("-Wdeprecated-declarations");
-        cxx_flags.push("-Wunused-but-set-variable");
-        cxx_flags.push("-Wextra");
-        cxx_flags.push("-Wpedantic");
-        cxx_flags.push("-Wcast-qual");
-        cxx_flags.push("-Wno-unused-function");
-        cxx_flags.push("-Wno-multichar");
+        cx.flag("-pthread")
+            .flag("-Wall")
+            .flag("-Wextra")
+            .flag("-Wpedantic")
+            .flag("-Wcast-qual")
+            .flag("-Wdouble-promotion")
+            .flag("-Wshadow")
+            .flag("-Wstrict-prototypes")
+            .flag("-Wpointer-arith");
+        cxx.flag("-fPIC")
+            .flag("-pthread")
+            .flag("-Wall")
+            .flag("-Wdeprecated-declarations")
+            .flag("-Wextra")
+            .flag("-Wpedantic")
+            .flag("-Wcast-qual")
+            .flag("-Wno-unused-function")
+            .flag("-Wno-multichar");
     } else if cfg!(target_family = "windows") {
-        cx_flags.push("/W4");
-        cx_flags.push("/Wall");
-        cx_flags.push("/wd4820");
-        cx_flags.push("/wd4710");
-        cx_flags.push("/wd4711");
-        cx_flags.push("/wd4820");
-        cx_flags.push("/wd4514");
+        cx.flag("/W4")
+            .flag("/Wall")
+            .flag("/wd4820")
+            .flag("/wd4710")
+            .flag("/wd4711")
+            .flag("/wd4820")
+            .flag("/wd4514");
+        cxx.flag("/W4")
+            .flag("/Wall")
+            .flag("/wd4820")
+            .flag("/wd4710")
+            .flag("/wd4711")
+            .flag("/wd4820")
+            .flag("/wd4514");
+    }
 
-        cxx_flags.push("/W4");
-        cxx_flags.push("/Wall");
-        cxx_flags.push("/wd4820");
-        cxx_flags.push("/wd4710");
-        cxx_flags.push("/wd4711");
-        cxx_flags.push("/wd4820");
-        cxx_flags.push("/wd4514");
+    if cfg!(target_os = "openbsd") {
+        cx.define("_XOPEN_SOURCE", "700");
+        cxx.define("_XOPEN_SOURCE", "700");
     } else {
-        unimplemented!("Other platforms are not yet supported")
+        cx.define("_XOPEN_SOURCE", "600");
+        cxx.define("_XOPEN_SOURCE", "600");
+    }
+
+    if cfg!(target_os = "linux") {
+        cx.define("_GNU_SOURCE", None);
+        cxx.define("_GNU_SOURCE", None);
+    } else if cfg!(any(
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "dragonfly"
+    )) {
+        cx.define("_DARWIN_C_SOURCE", None);
+        cxx.define("_DARWIN_C_SOURCE", None);
+    } else if cfg!(target_os = "openbsd") {
+        cx.define("_BSD_SOURCE", None);
+        cxx.define("_BSD_SOURCE", None);
+    } else if cfg!(target_os = "freebsd") {
+        cx.define("__BSD_VISIBLE", None);
+        cxx.define("__BSD_VISIBLE", None);
+    } else if cfg!(target_os = "netbsd") {
+        cx.define("_NETBSD_SOURCE", None);
+        cxx.define("_NETBSD_SOURCE", None);
     }
 
     if cfg!(any(target_arch = "arm", target_arch = "aarch64")) {
         if cfg!(target_family = "unix") {
-            cx_flags.push("-mavx512vnni");
-            cx_flags.push("-mfp16-format=ieee");
-            cxx_flags.push("-mavx512vnni");
-            cxx_flags.push("-mfp16-format=ieee");
-        } else {
-            unimplemented!("Other ARM platforms are not yet supported")
+            cx.flag("-mavx512vnni").flag("-mfp16-format=ieee");
+            cxx.flag("-mavx512vnni").flag("-mfp16-format=ieee");
+        } else if cfg!(target_family = "windows") {
+            cx.define("__ARM_NEON", None)
+                .define("__ARM_FEATURE_FMA", None)
+                .define("__ARM_FEATURE_DOTPROD", None)
+                .define("__aarch64__", None);
+            cxx.define("__ARM_NEON", None)
+                .define("__ARM_FEATURE_FMA", None)
+                .define("__ARM_FEATURE_DOTPROD", None)
+                .define("__aarch64__", None);
         }
     }
 }
 
-/// Add platform appropriate flags based on enabled features.
-fn push_feature_flags(cx_flags: &mut Vec<&str>, cxx_flags: &mut Vec<&str>) {
+/// Add platform appropriate flags and definitions based on enabled features.
+fn push_feature_flags(cx: &mut Build, cxx: &mut Build) {
     // TODO in llama.cpp's cmake (https://github.com/ggerganov/llama.cpp/blob/9ecdd12e95aee20d6dfaf5f5a0f0ce5ac1fb2747/CMakeLists.txt#L659), they include SIMD instructions manually, however it doesn't seem to be necessary for VS2022's MSVC, check when it is needed
 
     if cfg!(any(target_arch = "x86", target_arch = "x86_64")) {
         if cfg!(feature = "native") && cfg!(target_family = "unix") {
-            cx_flags.push("-march=native");
-            cxx_flags.push("-march=native");
+            cx.flag("-march=native");
+            cxx.flag("-march=native");
         }
 
         if cfg!(feature = "fma") && cfg!(target_family = "unix") {
-            cx_flags.push("-mfma");
-            cxx_flags.push("-mfma");
+            cx.flag("-mfma");
+            cxx.flag("-mfma");
         }
 
         if cfg!(feature = "f16c") && cfg!(target_family = "unix") {
-            cx_flags.push("-mf16c");
-            cxx_flags.push("-mf16c");
+            cx.flag("-mf16c");
+            cxx.flag("-mf16c");
         }
 
         if cfg!(target_family = "unix") {
             if cfg!(feature = "avx512") {
-                cx_flags.push("-mavx512f");
-                cx_flags.push("-mavx512bw");
-                cxx_flags.push("-mavx512f");
-                cxx_flags.push("-mavx512bw");
+                cx.flag("-mavx512f").flag("-mavx512bw");
+                cxx.flag("-mavx512f").flag("-mavx512bw");
 
                 if cfg!(feature = "avx512_vmbi") {
-                    cx_flags.push("-mavx512vbmi");
-                    cxx_flags.push("-mavx512vbmi");
+                    cx.flag("-mavx512vbmi");
+                    cxx.flag("-mavx512vbmi");
                 }
 
                 if cfg!(feature = "avx512_vnni") {
-                    cx_flags.push("-mavx512vnni");
-                    cxx_flags.push("-mavx512vnni");
+                    cx.flag("-mavx512vnni");
+                    cxx.flag("-mavx512vnni");
                 }
             }
 
             if cfg!(feature = "avx2") {
-                cx_flags.push("-mavx2");
-                cxx_flags.push("-mavx2");
+                cx.flag("-mavx2");
+                cxx.flag("-mavx2");
             }
 
             if cfg!(feature = "avx") {
-                cx_flags.push("-mavx");
-                cxx_flags.push("-mavx");
+                cx.flag("-mavx");
+                cxx.flag("-mavx");
             }
         } else if cfg!(target_family = "windows") {
             if cfg!(feature = "avx512") {
-                cx_flags.push("/arch:AVX512");
-                cxx_flags.push("/arch:AVX512");
+                cx.flag("/arch:AVX512");
+                cxx.flag("/arch:AVX512");
 
                 if cfg!(feature = "avx512_vmbi") {
-                    cx_flags.push("/D__AVX512VBMI__");
-                    cxx_flags.push("/D__AVX512VBMI__");
+                    cx.define("__AVX512VBMI__", None);
+                    cxx.define("__AVX512VBMI__", None);
                 }
 
                 if cfg!(feature = "avx512_vnni") {
-                    cx_flags.push("/D__AVX512VNNI__");
-                    cxx_flags.push("/D__AVX512VNNI__");
+                    cx.define("__AVX512VNNI__", None);
+                    cxx.define("__AVX512VNNI__", None);
                 }
             } else if cfg!(feature = "avx2") {
-                cx_flags.push("/arch:AVX2");
-                cxx_flags.push("/arch:AVX2");
+                cx.flag("/arch:AVX2");
+                cxx.flag("/arch:AVX2");
             } else if cfg!(feature = "avx") {
-                cx_flags.push("/arch:AVX");
-                cxx_flags.push("/arch:AVX");
+                cx.flag("/arch:AVX");
+                cxx.flag("/arch:AVX");
             }
         }
     }
@@ -370,55 +398,19 @@ fn compile_vulkan(cxx: &mut Build) {
         .define("GGML_USE_VULKAN", None);
 }
 
-fn compile_ggml(cx: &mut Build, cx_flags: &Vec<&str>) {
+fn compile_ggml(mut cx: Build) {
     println!("Compiling GGML..");
-    for cx_flag in cx_flags {
-        cx.flag(cx_flag);
-    }
-
-    #[cfg(target_os = "linux")]
-    {
-        cx.define("_GNU_SOURCE", None);
-    }
-
     cx.include(LLAMA_PATH.as_path())
         .file(LLAMA_PATH.join("ggml.c"))
         .file(LLAMA_PATH.join("ggml-alloc.c"))
         .file(LLAMA_PATH.join("ggml-backend.c"))
         .file(LLAMA_PATH.join("ggml-quants.c"))
-        .cpp(false)
-        .static_flag(true)
-        .define("_XOPEN_SOURCE", "600")
-        .define("GGML_USE_K_QUANTS", None)
         .compile("ggml");
 }
 
-fn compile_llama(
-    cxx: &mut Build,
-    cxx_flags: &Vec<&str>,
-    _out_path: impl AsRef<Path>,
-    _ggml_type: &str,
-) {
+fn compile_llama(mut cxx: Build, _out_path: impl AsRef<Path>, _ggml_type: &str) {
     println!("Compiling Llama.cpp..");
-    for cxx_flag in cxx_flags {
-        cxx.flag(cxx_flag);
-    }
-
-    #[cfg(any(target_os = "macos", target_os = "ios", target_os = "dragonfly"))]
-    {
-        cxx.define("_DARWIN_C_SOURCE", None);
-    }
-
-    #[cfg(target_os = "linux")]
-    {
-        cxx.define("_GNU_SOURCE", None);
-    }
-
-    cxx.static_flag(true)
-        .file(LLAMA_PATH.join("llama.cpp"))
-        .cpp(true)
-        .define("_XOPEN_SOURCE", "600")
-        .compile("llama");
+    cxx.file(LLAMA_PATH.join("llama.cpp")).compile("llama");
 }
 
 fn main() {
@@ -433,15 +425,11 @@ fn main() {
 
     compile_bindings(&out_path);
 
-    let mut cx_flags = vec![];
-    let mut cxx_flags = vec![];
-
-    push_common_flags(&mut cx_flags, &mut cxx_flags);
-    push_feature_flags(&mut cx_flags, &mut cxx_flags);
-
     let mut cx = Build::new();
-
     let mut cxx = Build::new();
+
+    push_common_flags(&mut cx, &mut cxx);
+    push_feature_flags(&mut cx, &mut cxx);
 
     let mut ggml_type = String::new();
 
@@ -483,9 +471,8 @@ fn main() {
         //
         // compile_llama(&mut cxx, &cxx_flags, &out_path, "cuda");
     } else {
-        compile_ggml(&mut cx, &cx_flags);
-
-        compile_llama(&mut cxx, &cxx_flags, &out_path, &ggml_type);
+        compile_ggml(cx);
+        compile_llama(cxx, &out_path, &ggml_type);
     }
 
     #[cfg(feature = "compat")]
@@ -679,7 +666,7 @@ mod compat {
             if let Ok(output) = Command::new(name).arg("--version").output() {
                 if output.status.success() {
                     let out_str = String::from_utf8_lossy(&output.stdout);
-                    println!("Valid \"tool\" found:\n{out_str}");
+                    println!("Valid tool found:\n{out_str}");
                     return Some(name);
                 }
             }
