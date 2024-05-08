a38635f8043985994cb01aecdd323db8a4ea3649
Ziyang Hu <hu.ziyang@cantab.net>
2024-05-08 22:23:32 +0800

    bump llama.cpp version

--- crates/llama_cpp_sys/include/build-info.h
+++ crates/llama_cpp_sys/include/build-info.h
@@ -13,7 +13,7 @@
 #ifndef BUILD_INFO_H
 #define BUILD_INFO_H
 
-#define BUILD_NUMBER 2589
-#define BUILD_COMMIT "60cdf40"
+#define BUILD_NUMBER 2815
+#define BUILD_COMMIT "911b3900"
 
 #endif // BUILD_INFO_H
